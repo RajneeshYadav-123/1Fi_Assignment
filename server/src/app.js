78187eddfa7f6ca589853746110b309ed9cbf44b
RajneeshYadav-123 <rajneeshyadav0507@gmail.com>
2026-09-03 21:26:39 +0530

    CORS dev

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,13 @@ const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+     'http://localhost:5173'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
